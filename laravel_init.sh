f5fc5aa7a12cf358afd9f603f11bf02e67b2b48a
Stephan MIller <stephanmil@gmail.com>
2017-03-04 23:21:45 -0600

    Build api folder if it doesn’t exist

--- laravel_init.sh
+++ laravel_init.sh
@@ -4,6 +4,14 @@ set -e
 #We will restart it in the forground at the end of the script
 service apache2 stop
 
+# Create api laravel project if it doesn't exist
+if [ ! -d /var/www/html/api ]; then
+    cd /var/www/html
+    composer create-project --prefer-dist laravel/laravel api
+    composer require yab/laracogs
+    php artisan vendor:publish --provider="Yab\Laracogs\LaracogsProvider"
+fi
+
 #Laravel log permissions
 sudo chown -R www-data:www-data /var/www/html/api/storage
 sudo chmod -R 777 /var/www/html/api/storage
